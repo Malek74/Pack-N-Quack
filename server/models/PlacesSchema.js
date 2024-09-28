2a8b81f96d7019edd52569bc770b613fe1452c60
7oorein <horeinayman@gmail.com>
2024-09-29 00:20:09 +0300

    Created places schema

---
+++ server/models/PlacesSchema.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+import { Schema } from "mongoose";
+
+const placesSchema = new Schema({
+    description: {
+        type: String,
+        required: true,
+    },
+    pictures: {
+        type: [String], // Array of strings for URLs or paths to pictures & see if we want to change it 
+        required: true,
+    },
+    location: {
+        type: String,
+        required: true,
+    },
+    opening_hour: {
+        type: String, // we can use Date to specify time with a date format
+        required: true,
+    },
+    ticket_price: {
+        type: Number, 
+        required: true,
+    },
+});
+
+const Places = mongoose.model("Places", placesSchema);
+
+export default Places;
