53453ecb00add954118f6d28d6823ceeeb900d73
Mariam-Maged2003 <mariammariammaged@gmail.com>
2024-10-02 17:27:51 +0300

    feat: admin dashboard, create users

--- client/src/router/routes.jsx
+++ client/src/router/routes.jsx
@@ -2,11 +2,13 @@ import { createRoutesFromElements, Route } from "react-router-dom";
 import HomePage from "@/pages/HomePage";
 import AboutPage from "@/pages/AboutPage";
 import Layout from "@/components/layout/Layout";
+import AdminPage from "@/pages/AdminPage";
 
 const routes = createRoutesFromElements(
   <Route path="/" element={<Layout />}>
     <Route index element={<HomePage />} />
     <Route path="about" element={<AboutPage />} />
+    <Route path="admin" element={<AdminPage />} />
   </Route>,
 );
 
