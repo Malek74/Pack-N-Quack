0fb180a671e7d7a97f1cc6cdc5ec6bcafeb28894
amir-tarek <128504023+amir-tarek@users.noreply.github.com>
2024-10-05 16:26:00 +0300

    fix: admin page forms

--- client/src/components/ActivityCategory.jsx
+++ client/src/components/ActivityCategory.jsx
@@ -21,90 +21,22 @@ import {
 } from "@/components/ui/dialog"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
-
-
+import CreateDialog from "./CreateDialog"
+import NewCategoryForm from "./forms/NewCategoryForm"
+import { EditCategoryDialog } from "./EditCategory"
 export default function ActivityCategory() {
     const deleteClicked = (id) => {
         console.log(id)
     }
     const activityCategories = [
         { id: 1, name: "food" }, { id: 2, name: "stand up comedy" }, { id: 3, name: "concert" }, { id: 4, name: "party" }]
-        
+    
     const CreateButton = ()=>{
         return(
-            <Dialog>
-            <DialogTrigger asChild>
-            <Button variant="outline"> Add Activity Category</Button>
-        </DialogTrigger>
-        <DialogContent className="sm:max-w-[425px]">
-            <DialogHeader>
-                <DialogTitle>Create a new activity category</DialogTitle>
-                <DialogDescription>
-                    Add a new activity category. Click create when you're done.
-                </DialogDescription>
-            </DialogHeader>
-            <div className="grid gap-4 py-4">
-                        <div className="grid grid-cols-4 items-center gap-4">
-                            <Label htmlFor="Activity name" className="text-right">
-                            Category name
-                            </Label>
-                            <Input
-                                id="Category name"
-                                defaultValue=""
-                                className="col-span-3"
-                            />
-                        </div>
-                        
-                    </div>
-                    <DialogFooter>
-                        <Button type="submit">Create</Button>
-                    </DialogFooter>
-            
-
-        </DialogContent>
-    </Dialog>
-
+            <CreateDialog title="Category" form={<NewCategoryForm />}/>
     )    
-  
         
     }    
-        
-    const EditButton = (props) => {
-        return (
-            
-            
-            <Dialog>
-                <DialogTrigger asChild>
-                    <Button variant="outline">Update </Button>
-                </DialogTrigger>
-                <DialogContent className="sm:max-w-[425px]">
-                    <DialogHeader>
-                        <DialogTitle>Update </DialogTitle>
-                        <DialogDescription>
-                            Edit the activity category. Click update when you're done.
-                        </DialogDescription>
-                    </DialogHeader>
-                    <div className="grid gap-4 py-4">
-                        <div className="grid grid-cols-4 items-center gap-4">
-                            <Label htmlFor="name" className="text-right">
-                                Category name
-                            </Label>
-                            <Input
-                                id="Category name"
-                                defaultValue={props.default} 
-                                className="col-span-3"
-                            />
-                        </div>
-                        
-                    </div>
-                    <DialogFooter>
-                        <Button type="submit">Update</Button>
-                    </DialogFooter>
-                </DialogContent>
-            </Dialog>
-            
-        )
-    }
 
     return (
         <Table>
@@ -129,11 +61,11 @@ export default function ActivityCategory() {
                         <TableCell className="font-medium">{activity.id}</TableCell>
                         <TableCell>{activity.name}</TableCell>
                         <TableCell>
-                            <EditButton default={activity.name}/>
+                            <EditCategoryDialog category={activity.name}/>
                         </TableCell>
                         <TableCell className="text-right">
                     <Button onClick={() => deleteClicked(account.id)} variant="destructive">
-                        <Trash2 className="mr-2 h-4 w-4" /> Delete Account
+                        <Trash2 className="mr-2 h-4 w-4" /> Delete
                     </Button>
                 </TableCell>
                         
---
+++ client/src/components/EditTag.jsx
@@ -0,0 +1,78 @@
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Pencil } from "lucide-react";
+
+// Define the schema for validation
+const categoryFormSchema = z.object({
+  tag: z.string().min(1, { message: "tag is required." }),
+});
+
+export function EditTagDialog({ tag }) {
+  // Initialize form with default values from the tag being edited
+  const form = useForm({
+    resolver: zodResolver(categoryFormSchema),
+    defaultValues: {
+      tag: tag ?? "", // Set the default value for tag
+    },
+  });
+
+  // Handle form submission
+  function onSubmit(values) {
+    console.log("tag Edited:", values.tag);
+  }
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button className="flex gap-2">
+          <Pencil /> <p> Edit </p>
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Edit tag</DialogTitle>
+        </DialogHeader>
+        {/* Edit tag Form */}
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4 flex flex-col">
+            {/* tag Field */}
+            <FormField
+              control={form.control}
+              name="Tag"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>tag</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Enter tag" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button className="place-self-end" type="submit">
+              Save Changes
+            </Button>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+}
