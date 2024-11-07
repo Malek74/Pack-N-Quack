4b6fb9745601bb3ed77692005f4e0569213a83e5
Youssef Ramy <youssef.rfarid@gmail.com>
2024-11-07 13:30:42 +0200

    transfer to laptop

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,9 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./style/main.css";
 import axios from "axios";
-axios.defaults.baseURL = "https://pack-n-quack-production-7f27.up.railway.app/";
+
+//axios.defaults.baseURL = "https://pack-n-quack-production-7f27.up.railway.app/";
+axios.defaults.baseURL = "http://localhost:8000/";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
--- client/src/pages/RamitoItineraries.jsx
+++ client/src/pages/RamitoItineraries.jsx
@@ -6,46 +6,132 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import { Button } from "@/components/ui/button";
 import RamitoItinerariesCard from "@/components/ramitoItineraries/ramitoCard";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import Loading from "@/components/shared/Loading";
 
 export default function RamitoItineraries() {
+  const [isLoading, setIsLoading] = useState(true);
   const [fetchedItineraries, setFetchedItineraries] = useState([]);
-  // const [fetchedItinerariesParams, setFetchedItinerariesParams] = useState({});
+  const [searchTerm, setSearchTerm] = useState("");
+  const [language, setLanguage] = useState("");
+  const [fetchedItineraryTags, setFetchedItineraryTags] = useState([]);
+  const [fetchedItinerariesParams, setFetchedItinerariesParams] = useState({
+    name: "",
+    language: "",
+  });
 
   useEffect(() => {
-    const fetchItineraries = async () => {
+    setFetchedItinerariesParams({ name: searchTerm, language: language });
+  }, [language, searchTerm]);
+
+  useEffect(() => {
+    const fetchItineraryTags = async () => {
       try {
-        const response = await axios.get("/api/itinerary");
-        console.log(response.data);
-        setFetchedItineraries(response.data);
+        const response = await axios.get("/api/itineraryTags");
+        setFetchedItineraryTags(response.data);
       } catch (error) {
         console.error(error);
       }
     };
-    fetchItineraries();
+
+    fetchItineraryTags();
   }, []);
+
+  useEffect(() => {
+    const fetchItineraries = async () => {
+      try {
+        setIsLoading(true);
+        const response = await axios.get("/api/itinerary", {
+          params: fetchedItinerariesParams,
+        });
+        setFetchedItineraries(response.data);
+        setIsLoading(false);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchItineraries();
+  }, [fetchedItinerariesParams]);
+
   return (
     <div className="flex flex-col w-screen p-14 pb-0">
       <div className="relative">
         <Banner background={BannerImage} name="Ramito Itineraries" />
-        <SearchBar />
+        <SearchBar
+          searchTerm={searchTerm}
+          setSearchTerm={setSearchTerm}
+          placeholder={"Look for an itinerary..."}
+        />
       </div>
 
-      <div className="grid grid-cols-3 justify-stretch w-screen self-center gap-10 px-14">
-        {fetchedItineraries &&
-          fetchedItineraries.map((itinerary) => (
-            <RamitoItinerariesCard
-              key={itinerary._id}
-              id={itinerary._id}
-              image={itinerary.image}
-              name={itinerary.name}
-              description={itinerary.description}
-              tags={itinerary.tags}
-              price={itinerary.price}
-              rating={itinerary.ratings.averageRating}
-              numberOfReviews={itinerary.ratings.reviews.length}
-            />
-          ))}
+      <div className="flex w-screen self-center justify-center gap-10 p-10 pt-0">
+        <Select
+          value={language}
+          onValueChange={(value) => setLanguage(value === "all" ? "" : value)}
+        >
+          <SelectTrigger className="w-[180px]">
+            <SelectValue placeholder="Select a Language" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectLabel>Languages</SelectLabel>
+              <SelectItem value="all">Any</SelectItem>
+              <SelectItem value="English">English</SelectItem>
+              <SelectItem value="Spanish">Spanish</SelectItem>
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+
+        <Select
+          value={language}
+          onValueChange={(value) => setLanguage(value === "all" ? "" : value)}
+        >
+          <SelectTrigger className="w-[180px]">
+            <SelectValue placeholder="Select a Language" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectLabel>Languages</SelectLabel>
+              <SelectItem value="all">Any</SelectItem>
+              <SelectItem value="English">English</SelectItem>
+              <SelectItem value="Spanish">Spanish</SelectItem>
+            </SelectGroup>
+          </SelectContent>
+        </Select>
       </div>
+
+      {isLoading && (
+        <div className="flex justify-center items-center">
+          <Loading size="xl" />
+        </div>
+      )}
+      {!isLoading && (
+        <div className="grid grid-cols-3 justify-stretch w-screen self-center gap-10 px-14">
+          {fetchedItineraries &&
+            fetchedItineraries.map((itinerary) => (
+              <RamitoItinerariesCard
+                key={itinerary._id}
+                id={itinerary._id}
+                image={itinerary.image}
+                name={itinerary.name}
+                description={itinerary.description}
+                tags={itinerary.tags}
+                price={itinerary.price}
+                rating={itinerary.ratings.averageRating}
+                numberOfReviews={itinerary.ratings.reviews.length}
+              />
+            ))}
+        </div>
+      )}
       <Button onClick={() => console.log(fetchedItineraries[0])}>
         Print Itineraries
       </Button>
