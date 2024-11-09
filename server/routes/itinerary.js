b01ff6814cbbe8d98f678554863ed798fe0b99de
Malek-0903 <malek.mohamed@si-ware.com>
2024-11-09 23:50:39 +0200

    fix: Correct import statement and ensure consistent formatting in itinerary routes

--- server/routes/itinerary.js
+++ server/routes/itinerary.js
@@ -1,11 +1,11 @@
 import express from "express";
-import  upload  from '../middleware/multer.js';
+import { upload } from '../middleware/multer.js';
 import cloudinary from "../utils/cloudinary.js";
 import { addItinerary, deleteItinerary, getMaxPrice, getItinerary, updateItinerary, getMyItineraries, getItineraryById, addActivity, getAllLanguages, Flagg, rateIternary, addImagesToItinerary, addCoverImageToItinerary, addImagesToActivity } from "../controllers/itineraryController.js";
 
 const router = express.Router();
 
-router.post("/", upload.fields([{ name: 'images', maxCount: 20 }, { name: 'coverImage', maxCount: 1 },{name: "activityImages", maxcount: 10}]), addItinerary);
+router.post("/", upload.fields([{ name: 'images', maxCount: 20 }, { name: 'coverImage', maxCount: 1 }, { name: "activityImages", maxcount: 10 }]), addItinerary);
 router.get("/", getItinerary);
 router.delete("/:id", deleteItinerary);
 router.put("/:id", updateItinerary);
@@ -15,7 +15,7 @@ router.get("/maxPrice", getMaxPrice);
 router.get("/languages", getAllLanguages);
 router.put("/updateActivityImage/:id", upload.fields([{ name: 'images', maxCount: 1 }]), addImagesToActivity);
 router.put("/updateCoverImage/:id", upload.fields([{ name: 'images', maxCount: 1 }]), addCoverImageToItinerary);
-router.put("/updateItineraryImages/:id", upload.fields([{ name: 'images', maxCount: 10 }]) , addImagesToItinerary);
+router.put("/updateItineraryImages/:id", upload.fields([{ name: 'images', maxCount: 10 }]), addImagesToItinerary);
 router.put("/flag/:id", Flagg);
 //router.put('/:id', ItineraryActivation);
 router.post("/addActivity/:id", addActivity);
