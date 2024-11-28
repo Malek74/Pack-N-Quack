0132cda253f2fe9e88c997feddcc198a02f81701
Gasser Ali <gasser.ali.emara@gmail.com>
2024-11-29 01:34:40 +0200

    added wishlist functions

--- server/routes/touristRoutes.js
+++ server/routes/touristRoutes.js
@@ -12,7 +12,10 @@ import {
     getMyHotels,
     viewMyTourGuides,
     viewMyItineraries,
-    viewMyActivities
+    viewMyActivities,
+    viewMyWishlist,
+    addToWishlist,
+    removeFromWishlist
 } from '../controllers/touristController.js';
 import { createComplaint, viewMyComplaints, viewComplaintById } from '../controllers/complaintController.js';
 
@@ -36,5 +39,8 @@ router.get('/myactivities/:id', viewMyActivities);
 router.get('/complaints/:id', viewMyComplaints);
 router.post('/complaints', createComplaint);
 router.get('/myComplaints/:id', viewComplaintById);
+router.get('/wishlist/:id', viewMyWishlist);
+router.post('/wishlist/:id', addToWishlist);
+router.delete('/wishlist/:id', removeFromWishlist);
 
 export default router;
