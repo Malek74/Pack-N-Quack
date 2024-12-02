8c2874b635bf079856dd878618fe4cea6cbe8803
Youssef Ramy <youssef.rfarid@gmail.com>
2024-12-02 23:33:02 +0200

    tweaked animation

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -69,15 +69,15 @@ module.exports = {
 			keyframes: {
 				shake: {
 					'0%': { transform: 'rotate(0deg) scale(1)' },
-					'10%': { transform: 'rotate(10deg) scale(1.1)' },
+					'10%': { transform: 'rotate(10deg) scale(1.3)' },
 					'20%': { transform: 'rotate(-10deg) scale(1)' },
-					'30%': { transform: 'rotate(10deg) scale(1.1)' },
+					'30%': { transform: 'rotate(10deg) scale(1.3)' },
 					'40%': { transform: 'rotate(-10deg) scale(1)' },
-					'50%': { transform: 'rotate(10deg) scale(1.1)' },
+					'50%': { transform: 'rotate(10deg) scale(1.3)' },
 					'60%': { transform: 'rotate(-10deg) scale(1)' },
-					'70%': { transform: 'rotate(10deg) scale(1.1)' },
+					'70%': { transform: 'rotate(10deg) scale(1.3)' },
 					'80%': { transform: 'rotate(-10deg) scale(1)' },
-					'90%': { transform: 'rotate(10deg) scale(1.1)' },
+					'90%': { transform: 'rotate(10deg) scale(1.3)' },
 					'100%': { transform: 'rotate(0deg) scale(1)' },
 				},
 				'accordion-down': {
