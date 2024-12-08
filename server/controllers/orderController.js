f535cab214b5392eaf0487d6a3c80beb83c6ba67
7oorein <horeinayman@gmail.com>
2024-12-08 19:59:19 +0200

    order should be complete missing gasser's part

---
+++ server/controllers/orderController.js
@@ -0,0 +1,218 @@
+import Tourist from "../models/touristSchema.js";
+import Product from "../models/productSchema.js";
+import PurchasedItem from "../models/purchasedSchema.js";
+import Stripe from "stripe";
+import Order from "../models/orderSchema.js"
+
+
+export const rateProduct = async (req, res) => {
+    const touristID = req.user._id;
+    const { productID, rating } = req.body;
+
+    try {
+        // Check if the product exists
+        const productExist = await Product.findById(productID);
+        if (!productExist) {
+            return res.status(404).json({ message: "Product doesn't exist" });
+        }
+
+        // Check if the user exists
+        const userExist = await Tourist.findById(touristID);
+        if (!userExist) {
+            return res.status(404).json({ message: "User doesn't exist" });
+        }
+
+        // Check if the user has purchased the product
+        const purchaseRecord = await Order.findOne({ touristID: touristID, "products.productID": productID });
+        if (!purchaseRecord) {
+            return res.status(404).json({ message: "You have not purchased this product, so you cannot rate it" });
+        }
+
+        // Validate the rating
+        if (rating < 0 || rating > 5) {
+            return res.status(400).json({ message: "Rating must be between 0 and 5" });
+        }
+
+        // Check the status of the specific purchased item
+        let item = purchaseRecord.products.find(item => item.productID.toString() === productID.toString());
+        if (item && item.orderStatus === "Pending") {
+            return res.status(400).json({ message: "You cannot rate a product that you haven't yet received" });
+        }
+
+        // Update the rating for the specific product in the user's purchase record
+        const updatedRecord = await Order.findOneAndUpdate(
+            { touristID: touristID, "products.productID": productID },
+            { $set: { "products.$.rating": rating } }, // Only updating the rating field
+            { new: true }
+        );
+
+        // Calculate the new average rating for the product
+        const newAverageRating =
+            ((productExist.ratings.averageRating * productExist.ratings.reviews.length) + rating) /
+            (1 + productExist.ratings.reviews.length);
+
+        // Update the product's rating and add a simple rating record (no review)
+        await Product.findByIdAndUpdate(
+            productID,
+            {
+                $set: { "ratings.averageRating": newAverageRating },
+                $push: { "ratings.reviews": { touristID: touristID, rating: rating } }
+            },
+            { new: true }
+        );
+
+        return res.status(200).json(updatedRecord);
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+};
+
+export const ReviewProduct = async (req, res) => {
+    const touristID = req.user._id;
+    const {  productID, review } = req.body;
+    console.log(req.body);
+    try {
+
+        const productExist = await Product.findById(productID);
+        if (!productExist) {
+            return res.status(404).json({ message: "Product doesn't exist" });
+        }
+
+        const userExist = await Tourist.findById(touristID);
+        if (!userExist) {
+            return res.status(404).json({ message: "User doesn't exist" });
+        }
+
+        const purchaseRecord = await Order.findOne({ touristID: touristID, "products.productID": productID });
+        if (!purchaseRecord) {
+            return res.status(404).json({ message: "You have not purchased this product, so you cannot review it" });
+        }
+
+        let item = purchaseRecord.products.find(item => item.productID.toString() === productID.toString());
+
+        if (item && item.orderStatus === "Pending" ) {
+            return res.status(400).json({ message: "You cannot review a product that you haven't yet received" });
+        }
+
+        const updatedRecord = await Order.findOneAndUpdate(
+            { touristID: touristID, "products.productID": productID },
+            { $set: { "products.$.review": review } },
+            { new: true }
+        );
+        console.log(updatedRecord);
+        return res.status(200).json(updatedRecord);
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+};
+
+export const viewAllOrderDetails = async (req, res) => {
+    const touristID = req.user._id; 
+    try {
+        const orderDetails = await Order.findOne({ touristID: touristID });
+
+        if (!orderDetails) {
+            return res.status(404).json({ message: "No orders found for this user." });
+        }
+
+        return res.status(200).json(orderDetails);
+    } catch (error) {
+        console.error("Error retrieving order details:", error);
+        return res.status(500).json({ message: error.message });
+    }
+};
+
+export const viewSingleOrderDetails = async (req, res) => {
+    const touristID = req.user._id;
+    const {productID}=req.body;
+    try {
+        const orderDetails = await Order.findOne({ touristID: touristID, "products.productID": productID }, { "products.$": 1 });
+       
+        if (!orderDetails) {
+            return res.status(404).json({ message: "No orders found for this user." });
+        }
+
+        return res.status(200).json(orderDetails);
+    } catch (error) {
+        console.error("Error retrieving order details:", error);
+        return res.status(500).json({ message: error.message });
+    }
+};
+
+export const viewPendingOrderDetails = async (req, res) => {
+    const touristID = req.user._id;
+
+    try {
+        // Find orders for the user with "Pending" status
+        const pendingOrder = await Order.findOne({ touristID: touristID, orderStatus: "Pending" });
+
+        if (!pendingOrder) {
+            return res.status(404).json({ message: "No pending orders found for this user." });
+        }
+
+        return res.status(200).json(pendingOrder);
+    } catch (error) {
+        console.error("Error retrieving pending order details:", error);
+        return res.status(500).json({ message: error.message });
+    }
+};
+
+export const cancelOrder = async (req, res) => {
+    const touristID = req.user._id;
+    const { orderID } = req.body;
+
+    try {
+    
+        const purchaseRecord = await Order.findOne({ touristID: touristID, _id: orderID });
+        if (!purchaseRecord) {
+            return res.status(404).json({ message: "Order not found." });
+        }
+
+        // Check if the order is eligible for cancellation
+        if (purchaseRecord.orderStatus !== "Pending") {
+            return res.status(400).json({ message: "The order cannot be canceled as it is not 'Pending'." });
+        }
+
+        // Process each product in the order
+        for (const item of purchaseRecord.products) {
+            const productID = item.productID;
+
+            
+            await Product.findByIdAndUpdate(
+                productID,
+                {
+                    $inc: { product_sales: -1, available_quantity: 1 },
+                },
+                { new: true }
+            );
+
+            
+            item.quantity -= 1;
+
+            // Remove the product from the order if quantity reaches 0
+            if (item.quantity <= 0) {
+                purchaseRecord.products = purchaseRecord.products.filter(
+                    (product) => product.productID.toString() !== productID.toString()
+                );
+            }
+        }
+
+        if (purchaseRecord.products.length === 0) {
+            purchaseRecord.orderStatus = "Cancelled";
+        }
+
+        
+        await purchaseRecord.save();
+
+        return res.status(200).json({ message: "Order has been cancelled successfully." });
+    } catch (error) {
+        console.error("Error canceling order:", error);
+        return res.status(500).json({ message: error.message });
+    }
+};
+
+
+
+
+
+
---
+++ server/models/orderSchema.js
@@ -0,0 +1,60 @@
+import { Schema, model } from "mongoose";
+// import product from "./productSchema";
+// import Tourist from "./touristSchema";
+
+const orderSchema = new Schema({
+    touristID: {
+        type: Schema.Types.ObjectId,
+        ref: 'Tourist',
+        required: true
+    },
+    products: [{
+        productID: {
+            type: Schema.Types.ObjectId,
+            ref: 'Product',
+            required: true
+        },
+        quantity: {
+            type: Number,
+            required: true
+        },
+        rating: { type: Number },
+        review: { type: String },
+    }],
+    stripeSessionID: {
+        type: String,
+        default: null,
+        required: true
+    },
+    orderDate: {
+        type: Date,
+        default: Date.now
+    },
+    orderStatus: {
+        type: String,
+        required: true,
+        default: "Pending"
+    },
+    orderTotal: {
+        type: Number,
+        required: true
+    },
+    deliveryDate: {
+        type: Date,
+        required: false
+    },
+    paidByWallet:{
+        type: Number,
+        default: 0,
+        required: true
+    },
+    paidByCard:{
+        type: Number,
+        default: 0,
+        required: true
+    },
+},{timestamps: true});
+
+const Order = model("Order", orderSchema);
+
+export default Order;
