f8e6448715c1424bb022cb7406fdad6d650dd046
Youssef Ramy <youssef.rfarid@gmail.com>
2024-12-09 07:57:30 +0200

    fadel nezabat el header

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,14 +8,14 @@ import axios from "axios";
 axios.defaults.withCredentials = true; // Ensure cookies are sent with every request
 // DO NOT REMOVE THIS LINE ^ YA MARINAAAA
 
-axios.defaults.baseURL = "http://localhost:8000/";
+//axios.defaults.baseURL = "http://localhost:8000/";
 
 // axios.defaults.baseURL = "https://5bbbq9p8-8000.euw.devtunnels.ms/";
 // axios.defaults.baseURL = "https://5bbbq9p8-3000.euw.devtunnels.ms/";
 
 // axios.defaults.baseURL = "https://k0gfbwb4-8000.euw.devtunnels.ms/";
 // axios.defaults.baseURL = "https://5bbbq9p8-8000.euw.devtunnels.ms/";
-//axios.defaults.baseURL = "https://dqbw8hmb-8000.uks1.devtunnels.ms/";
+axios.defaults.baseURL = "https://dqbw8hmb-8000.uks1.devtunnels.ms/";
 // axios.defaults.baseURL = "https://k0gfbwb4-8000.euw.devtunnels.ms/";
 //axios.defaults.baseURL = "https://t18zlb13-8000.uks1.devtunnels.ms/";
 
